fc5fa12990388c80e544910f1bb6f36c738da776
Adam <pitoab@wp.pl>
2018-04-17 21:22:42 +0200

    dodanie nr tel i emaila po kliknięcu

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//show nr phone after clicking
+function showNrTel(){
+  document.getElementById("nrTel").innerHTML = "Tel +48 880 609 448";
+}
+function showEMail(){
+  document.getElementById("eMail").innerHTML = "bialachowski.adam@wp.pl";
+}
